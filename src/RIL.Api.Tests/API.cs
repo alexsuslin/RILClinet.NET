834f78e8568ace552443e64461d16d602de196be
Alex Suslin <vilebox@gmail.com>
2012-01-16 20:09:38 +0300

    secured data now in app.config, please change it as you need

--- src/RIL.Api.Tests/API.cs
+++ src/RIL.Api.Tests/API.cs
@@ -3,72 +3,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RIL.Api.Tests
 {
-    /// <summary>
-    /// Summary description for UnitTest1
-    /// </summary>
     [TestClass]
     public class API
     {
-        private const string RILToken = "";
-        readonly RIL Api = new RIL(RILToken);
+        readonly RIL Api = new RIL(Config.ApiKey);
 
         public API()
         {
-            Api.Credentials = new Credentials("name", "123");
+            Api.Credentials = new Credentials(Config.Username, Config.Password);
         }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         public void Auth()
         {
-            Assert.AreEqual(Api.Auth("name", "111").Status, Status.IncorrectCredentials);
-            Assert.AreEqual(Api.Auth("name", "123").Status, Status.Success);
+            Assert.AreEqual(Api.Auth(Config.Username, "WRONG PASSWORD").Status, Status.IncorrectCredentials);
+            Assert.AreEqual(Api.Auth(Config.Username, Config.Password).Status, Status.Success);
         }
 
         [TestMethod]
         public void ApiStatus()
         {
             Assert.AreEqual(Api.ApiStatus().isOk, true);
-            Assert.AreEqual(new RIL("invalidtoken", new Credentials("name", "123")).ApiStatus().isOk, false);
+            Assert.AreEqual(new RIL("INVALID API KEY", new Credentials(Config.Username, Config.Password)).ApiStatus().isOk, false);
         }
 
         [TestMethod]
@@ -81,7 +37,7 @@ namespace RIL.Api.Tests
         [TestMethod]
         public void RegisterNewUser()
         {
-            RILResponse responseExistingUser = Api.RegisterNewUser(new Credentials("name", "name"));
+            RILResponse responseExistingUser = Api.RegisterNewUser(new Credentials(Config.Username, Config.Password));
             Assert.AreEqual(responseExistingUser.Status, Status.IncorrectCredentials, "User with such name shoudl exists");
 
             //RILResponse responseNewUser = Api.RegisterNewUser(new Credentials("rilapitest8705", "rilapitest8705"));
---
+++ src/RIL.Api.Tests/Config.cs
@@ -0,0 +1,16 @@
+﻿namespace RIL.Api.Tests
+{
+    using AppConfig = System.Configuration.ConfigurationManager;
+
+    internal static class Config
+    {
+        #region Properties
+
+        internal static string ApiKey = AppConfig.AppSettings["ApiKey"] ?? "<INSERT API KEY>";
+        internal static string Username = AppConfig.AppSettings["Username"] ?? "<INSERT USERNAME>";
+        internal static string Password = AppConfig.AppSettings["Password"] ?? "<INSERT PASSWORD>";
+
+        #endregion
+    }
+
+}
